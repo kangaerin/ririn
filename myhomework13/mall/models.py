e7d04aaff9317ae127ea150f61d28022e47f34ab
kangaerin <95070081+kangaerin@users.noreply.github.com>
2021-12-01 18:10:17 +0900

    mall 모델, Shop 내용 생성 및 마이그레이션

---
+++ myhomework13/mall/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Shop(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    photo = models.ImageField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
