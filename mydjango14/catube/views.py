4b8117ca8e6b3f5b51cdaab84a5e0c43a85cb13a
kangaerin <95070081+kangaerin@users.noreply.github.com>
2021-12-02 16:27:23 +0900

    django-imagekit을 활용한 라이브러리 썸네일 생성

--- mydjango14/catube/views.py
+++ mydjango14/catube/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from catube.models import Video
+from django.views.generic import ListView
 
 
+# Class Based View(CBV) 클래스 기반의 view
+# index = ListView.as_view(model=Video, template_name="catube/index.html")
+
+# Fuction Based View (FBV) 함수기반 view
 def index(request: HttpRequest) -> HttpResponse:
     qs = Video.objects.all()
     return render(
@@ -10,5 +15,16 @@ def index(request: HttpRequest) -> HttpResponse:
         "catube/index.html",
         {
             "video_list": qs,
+        },
+    )
+
+
+def video_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    video = Video.objects.get(pk=pk)
+    return render(
+        request,
+        "catube/video_detail.html",
+        {
+            "video": video,
         },
     )
