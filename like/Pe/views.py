00752c01d5c87c4b005e98e9db51d281b8d7d817
kangaerin <95070081+kangaerin@users.noreply.github.com>
2021-12-05 20:13:16 +0900

    like index 뷰 구현

--- like/Pe/views.py
+++ like/Pe/views.py
@@ -1,2 +1,7 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request, "Pe/index.html")
 
--- like/like/urls.py
+++ like/like/urls.py
@@ -2,9 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from Pe.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('Pe/', index),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
