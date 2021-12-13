bf67d685cded2b2b9ab2a782f4b2bb4aa63530b6
kangaerin <95070081+kangaerin@users.noreply.github.com>
2021-12-13 14:00:11 +0900

    shop_list 뷰에 카테고리 검색기능 추가

--- mydjango20/shop/views.py
+++ mydjango20/shop/views.py
@@ -1,18 +1,24 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from shop.forms import ShopForm
-from shop.models import Shop
+from shop.models import Shop, Category
 
 
 # /shop/100/
 def shop_list(request: HttpRequest) -> HttpResponse:
+    category_qs = Category.objects.all()
     qs = Shop.objects.all()
+
+    category_id: str = request.GET.get("category_id", "")
+    if category_id:
+        qs = qs.filter(category__pk=category_id)
 
     query = request.GET.get("query", "")
     if query:
         qs = qs.filter(name__icontains=query)
 
     return render(request, "shop/shop_list.html", {
+        "category_list": category_qs,
         "shop_list": qs,
     })
 
