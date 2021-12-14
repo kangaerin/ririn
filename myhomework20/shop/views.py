f545e746a07878d32e5bfe81a9d149b2114a1256
kangaerin <95070081+kangaerin@users.noreply.github.com>
2021-12-14 22:38:23 +0900

    post_list, post_detail.html 구현

--- myhomework20/shop/views.py
+++ myhomework20/shop/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from shop.models import Post
+
+
+def post_list(request: HttpRequest) -> HttpResponse:
+    qs = Post.objects.all()
+    query = request.GET.get("query", "")
+    if query:
+        qs = qs.filter(title__icontains=query)
+
+    return render(request, "shop/post_list.html",{
+        "post_list" : qs,
+    })
+
+
+def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    post = get_object_or_404(Post, pk=pk)
+    # comment_list = post.comment_set.all()
+    # tag_list = post.tag_set.all()
+    return render(request, "shop/post_detail.html", {
+        "post": post,
+        # "comment_list": comment_list,
+        # "tag_list": tag_list,
+    })
