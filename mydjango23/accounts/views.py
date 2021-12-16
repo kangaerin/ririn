c129df83318dd8ffb08fe8056800f78e4f239e75
kangaerin <95070081+kangaerin@users.noreply.github.com>
2021-12-16 16:04:33 +0900

    로그인, 프로필, 로그아웃 view 구현

--- mydjango23/accounts/views.py
+++ mydjango23/accounts/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
@@ -16,8 +18,9 @@ def profile_image(request: HttpRequest) -> HttpResponse:
     return response
 
 
-def login(request):
-    pass
+login = LoginView.as_view(
+    template_name="accounts/login_form.html",
+)
 
 
 # 새로운 User 인스턴스를 만드는 것.
@@ -43,9 +46,11 @@ def signup(request):
 # )
 
 
+@login_required
 def profile(request):
-    pass
+    return render(request, "accounts/profile.html")
 
 
-def logout(request):
-    pass
+logout = LogoutView.as_view(
+    next_page="accounts:login",
+)
