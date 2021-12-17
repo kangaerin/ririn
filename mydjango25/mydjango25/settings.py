ac01fa325621537ecae1c1707548d2bfc436b96a
kangaerin <95070081+kangaerin@users.noreply.github.com>
2021-12-17 10:33:56 +0900

    django-bootstrap-v5 추가

--- mydjango25/mydjango25/settings.py
+++ mydjango25/mydjango25/settings.py
@@ -17,12 +17,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third apps
+    'bootstrap5',
+    # local apps
 ]
 
 if DEBUG:
@@ -119,5 +123,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#django-debug-toolbar
+# django-debug-toolbar
 INTERNAL_IPS = ['127.0.0.1']
