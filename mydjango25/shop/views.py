b864b08fdcfeb344365eb69e85e4a0294f861aa6
kangaerin <95070081+kangaerin@users.noreply.github.com>
2021-12-17 16:40:21 +0900

    review_edit 뷰를 CBV로 구현

--- mydjango25/shop/views.py
+++ mydjango25/shop/views.py
@@ -1,10 +1,11 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.http import HttpResponse
-from django.shortcuts import redirect, get_object_or_404
+from django.shortcuts import redirect, get_object_or_404, resolve_url
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from shop.forms import ReviewForm
+from shop.mixins import ReviewUserCheckMixin
 from shop.models import Shop, Category, Review
 
 
@@ -28,8 +29,10 @@ shop_detail = DetailView.as_view(
 class ReviewCreateView(LoginRequiredMixin, CreateView):
     model = Review
     form_class = ReviewForm
+
     # FIXME : shop detail로 이동
-    success_url = reverse_lazy("shop:shop_list")
+    # form_valid에서 직접 url 이동을 했기 때문에 아래 success_url은 불필요.
+    # success_url = reverse_lazy("shop:shop_list")
 
     # 유효성 검사에 통과한다면...
     def form_valid(self, form) -> HttpResponse:
@@ -47,3 +50,20 @@ class ReviewCreateView(LoginRequiredMixin, CreateView):
 
 
 review_new = ReviewCreateView.as_view()
+
+
+# 다중 상속(부모가 둘) LoginRequiredMixin, UpdateView
+# 파이썬은 다중 상속을 지원함.(지원하지 않는 언어도 있음)
+class ReviewUpdateView(LoginRequiredMixin, ReviewUserCheckMixin, UpdateView):
+    model = Review
+    form_class = ReviewForm
+
+    # FIXME: shop detail로 보내기
+    # success_url = reverse_lazy("shop:shop_list")
+
+    def get_success_url(self) -> str:
+        review = self.object
+        return resolve_url(review.shop)
+
+
+review_edit = ReviewUpdateView.as_view()
