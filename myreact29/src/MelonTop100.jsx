67beec2026de25f0ee200050d08c4c81ff7a7326
kangaerin <95070081+kangaerin@users.noreply.github.com>
2021-12-23 14:36:02 +0900

    song list 초기값을 json 파일로부터 로딩

--- myreact29/src/MelonTop100.jsx
+++ myreact29/src/MelonTop100.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Button } from 'react-bootstrap';
 import initialSong_List from './data/melon_data.json';
 import './MelonTop100.css';
 
@@ -15,7 +16,7 @@ function MelonTop100() {
   return (
     <div>
       <h2>멜론 top 100</h2>
-      <button onClick={handleClick}>로딩</button>
+      <Button onClick={handleClick}>로딩</Button>
       <ul className="songList">
         {songList.map((song) => {
           return (
