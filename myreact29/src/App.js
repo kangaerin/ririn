a80b32f40fd4e02852e9f2c12e69e373ef11399c
kangaerin <95070081+kangaerin@users.noreply.github.com>
2021-12-23 15:53:24 +0900

    MelonSearch 기본 컴포넌트 구성

--- myreact29/src/App.js
+++ myreact29/src/App.js
@@ -1,14 +1,18 @@
-import Counter from './Counter';
-import MelonTop100 from './MelonTop100';
+// import Counter from './Counter';
+// import MelonTop100 from './MelonTop100';
+import MelonSearch from './MelonSearch';
+
+import MelonTop100 from 'MelonTop100';
 
 function App() {
   return (
     <div>
       <h1>안녕 리액트!!</h1>
-      <MelonTop100 />
+      <MelonSearch />
+      {/* <MelonTop100 />
       <Counter />
       <Counter />
-      <Counter />
+      <Counter /> */}
     </div>
   );
 }
