dc2e83c9e7e66c4311aad6b2b84ca2c3bba6f3ae
kangaerin <95070081+kangaerin@users.noreply.github.com>
2022-01-04 11:34:08 +0900

    myhomework 과제풀이

--- myhomework35/src/SevenNumbers2.jsx
+++ myhomework35/src/SevenNumbers2.jsx
@@ -1,38 +1,43 @@
-import { useReducer } from "react";
-import Circle from "./Circle";
-import { zip, get_random_lotto_numbers, shuffle_array } from "./utils";
+import { useReducer } from 'react';
+import Circle from './Circle';
+import { zip, get_random_lotto_numbers, shuffle_array } from './utils';
 
 const INITIAL_STATE = {
   numbers: [0, 0, 0, 0, 0, 0, 0],
   colors: [
-    "#1B62BF",
-    "#1755A6",
-    "#37A647",
-    "#F29F05",
-    "#F23838",
-    "purple",
-    "pink",
+    '#1B62BF',
+    '#1755A6',
+    '#37A647',
+    '#F29F05',
+    '#F23838',
+    'purple',
+    'pink',
   ],
 };
 
+// 순수함수
 function reducer(prevState, action) {
+  // 새로운 상태값을 계산하여 리턴함
   const { type } = action;
-  if (type === "GENERATE_NUMBERS") {
+  if (type === 'GENERATE_NUMBERS') {
     return {
       ...prevState,
       numbers: get_random_lotto_numbers(),
     };
-  } else if (type === "SHUFFLE_NUMBERS") {
+  } else if (type === 'SHUFFLE_NUMBERS') {
     return {
       ...prevState,
       numbers: shuffle_array(prevState.numbers),
     };
-  } else if (type === "SHUFFLE_COLORS") {
+  } else if (type === 'SHUFFLE_COLORS') {
     return {
       ...prevState,
       colors: shuffle_array(prevState.colors),
     };
   }
+
+  //
+  return prevState;
 }
 
 function SevenNumbers1({ title }) {
@@ -45,13 +50,13 @@ function SevenNumbers1({ title }) {
         <Circle number={number} backgroundColor={color} />
       ))}
       <hr />
-      <button onClick={() => dispatch({ type: "GENERATE_NUMBERS" })}>
+      <button onClick={() => dispatch({ type: 'GENERATE_NUMBERS' })}>
         GENERATE_NUMBERS
       </button>
-      <button onClick={() => dispatch({ type: "SHUFFLE_NUMBERS" })}>
+      <button onClick={() => dispatch({ type: 'SHUFFLE_NUMBERS' })}>
         SHUFFLE_NUMBERS
       </button>
-      <button onClick={() => dispatch({ type: "SHUFFLE_COLORS" })}>
+      <button onClick={() => dispatch({ type: 'SHUFFLE_COLORS' })}>
         SHUFFLE_COLORS
       </button>
     </div>
